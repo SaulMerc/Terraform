e0ab511b00b07d4af87b927f702e0ba26b14b7c7
SaulMerc <saul.mercado9192@alumnos.udg.mx>
2024-06-05 18:44:54 -0600

    [TF_1.0] Basic structure of terraform

---
+++ 1-basic/main.tf
@@ -0,0 +1,29 @@
+#Initial format for terraform#
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+#Recommend don't leave access and secret key#
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+#Create an aws instace#
+resource "aws_instance" "example" {
+  ami = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServer"
+  }
+}
+#Use terraform plan to see the changes and all the infraestructure that will be create#
+#Use terraform apply to execute and create the infraetructure: with -auto-approve you accept directly all the changes#
+#Use terraform validate to make sure the configuration syntactically valid and internally consistent#
